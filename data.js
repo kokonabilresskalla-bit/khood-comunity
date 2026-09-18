09336ab97b436738455d582aae24001181c6d8c0
kokonabilresskalla-bit <kokonabilresskalla@gmail.com>
2026-09-18 21:04:55 +0300

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ window.KHOOD = window.KHOOD || {};
 KHOOD.GH = {
   owner: "kokonabilresskalla-bit",
   repo: "khood-comunity",
-  token: "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"
+  token: "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"
 };
 KHOOD.CHARS = [
   { id: "fady", name: "فادي", img: "./char-fady.png", pass: "F7#qL29vXm4P" },
